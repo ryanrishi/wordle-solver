9948f6dd7510dff859a5f391898d8d54413dad62
Ryan Rishi <ryan@ryanrishi.com>
2022-02-03 19:09:05 -0800

    Add parameterized tests for real Wordle solutions (#2)
    * add parameterized test with actual wordle answers
    
    * rename test
    
    * sort answers.csv by game number, asc

---
+++ src/test/java/com/ryanrishi/wordle/solver/RealWordleSolverTest.java
@@ -0,0 +1,47 @@
+package com.ryanrishi.wordle.solver;
+
+import com.ryanrishi.wordle.Wordle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Uses list of real Wordle solutions to test each solver
+ */
+public class RealWordleSolverTest {
+    Set<String> words = new LinkedHashSet<>();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/answers.csv", numLinesToSkip = 1)
+    void testBruteForceSolver(int gameNumber, String answer) {
+        answer = answer.toLowerCase();
+        Wordle wordle = new Wordle(answer);
+        Solver solver = new BruteForceSolver();
+        assertEquals(answer, solver.solve(wordle));
+        System.out.printf("'%s' (#%d) brute force: %d%n", answer, gameNumber, wordle.getNumGuesses());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/answers.csv", numLinesToSkip = 1)
+    void testIterativeSolver(int gameNumber, String answer) {
+        answer = answer.toLowerCase();
+        Wordle wordle = new Wordle(answer);
+        Solver solver = new IterativeSolver();
+        assertEquals(answer, solver.solve(wordle));
+        System.out.printf("'%s' (#%d) iterative: %d%n", answer, gameNumber, wordle.getNumGuesses());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/answers.csv", numLinesToSkip = 1)
+    void testIterativeFrequencySolver(int gameNumber, String answer) {
+        answer = answer.toLowerCase();
+        Wordle wordle = new Wordle(answer);
+        Solver solver = new IterativeSolverWithWordFrequency();
+        assertEquals(answer, solver.solve(wordle));
+        System.out.printf("'%s' (#%d) iterative + frequency: %d%n", answer, gameNumber, wordle.getNumGuesses());
+    }
+}
